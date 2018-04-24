0467593d56f53da1d0ac645ab052a10522b47e1d
ReMinoer <rmorillon@freneticexperience.com>
2018-04-24 22:55:38 +0200

    New Appveyor build name and add tests

---
+++ Tests/AppVeyorTest.Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AppVeyorTest.Test
+{
+    [TestFixture]
+    public class MyTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var obj = new Class1();
+            Assert.IsTrue(obj.Method());
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            var obj = new AppVeyorTestBis.Class1();
+            Assert.IsTrue(obj.Method());
+        }
+    }
+}
